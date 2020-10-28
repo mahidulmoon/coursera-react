69b9579fa2c2a8b56efbdde23132efbf31c844a9
mahidulmoon <mahidulmoon@gmail.com>
2020-10-28 22:24:24 +0600

    let vs var

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+function sayHello(){
+  // for (var i = 0; i<5 ;i++){ //var is only use for the same function
+  //   console.log(i)
+  // }
+  // console.log(i);
+  for (let i = 0; i<5 ;i++){
+    console.log(i)
+  }
+  //console.log(i);
+}
+
+sayHello();
