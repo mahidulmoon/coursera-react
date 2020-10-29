89f6dcd074bdf13b443f9579bd78462151af2848
mahidulmoon <mahidulmoon@gmail.com>
2020-10-29 11:39:45 +0600

    styling with jsx expression

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,17 +3,23 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
     formatcount(){
         const {count} = this.state;
         const z = <h1>Zero</h1>;
         return count === 0 ? z : count;
-    }
+    };
+    styles = {
+        fontSize : 10,
+        fontWeight : "bold"
+    };
     render() {
         return (
             <>
-                <span>{this.formatcount()}</span>
-                <button>Increment</button>
+                <img src={this.state.imageUrl} alt="" />
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatcount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </>
         );
     }
