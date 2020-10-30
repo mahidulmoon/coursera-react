c8485cd1d59947c497d3947db50a46d8b596e8b7
mahidulmoon <mahidulmoon@gmail.com>
2020-10-30 15:25:08 +0600

    deleting from state using js filter

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -4,6 +4,10 @@ class Movies extends Component {
     state ={
         movies:getMovies()
     }
+    hadleDelete = (movie) =>{
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    }
     render() {
         return (
             <div>
@@ -14,15 +18,17 @@ class Movies extends Component {
                             <th>Genre</th>
                             <th>Stock</th>
                             <th>Rate</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.state.movies.map(movie => (
-                            <tr>
+                            <tr key={movie.id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td><button onClick={()=>this.hadleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                             </tr>
                         ))}
                     </tbody>
