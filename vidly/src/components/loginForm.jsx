ac9a8ef0397b3598e8fc6bba76a9426ab3396fcf
mahidulmoon <mahidulmoon@gmail.com>
2020-11-04 16:01:43 +0600

    input value with state(object spread)

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 export default class LoginForm extends Component {
+    state={
+        account: {
+            username:'',
+            password:''
+        },
+
+    };
     username = React.createRef();
 
     // componentDidMount(){
@@ -11,17 +18,26 @@ export default class LoginForm extends Component {
         e.preventDefault();
         const username = this.username.current.value;
     }
+
+    handleChange = e =>{
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({account});
+    };
+
     render() {
         return (
         <div>
             <h1>Login</h1>
             <form onSubmit={this,this.handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="username">Username</label><input autoFocus ref={this.username} id="username" type="text" className="from-consol"/>
+                        <label htmlFor="username">Username</label>
+                        <input autoFocus ref={this.username} onChange={this.handleChange} value={this.state.account.username} id="username" type="text" className="from-consol"/>
                     </div>
             
                     <div className="form-group">
-                        <label htmlFor="password">Password</label><input id="password" type="text" className="from-consol"/>
+                        <label htmlFor="password">Password</label>
+                        <input value={this.state.account.password} id="password" type="text" className="from-consol"/>
                     </div>
                     <button className="btn btn-primary">Login</button>
             </form>
