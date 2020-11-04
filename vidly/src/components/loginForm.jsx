3aaf33c3e18d95674bc2d4cd1cc5957d30ae6c96
mahidulmoon <mahidulmoon@gmail.com>
2020-11-04 15:55:56 +0600

    input value with ref(not proper way)

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 
 export default class LoginForm extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Login</h1>
-          <form>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label><input id="username" type="text" className="from-consol"/>
-                </div>
-          
-                <div className="form-group">
-                    <label htmlFor="password">Password</label><input id="password" type="text" className="from-consol"/>
-                </div>
-                <button className="btn btn-primary">Login</button>
-          </form>
-      </div>
+    username = React.createRef();
+
+    // componentDidMount(){
+    //     this.username.current.focus();
+    // }
+
+    handleSubmit = e =>{
+        e.preventDefault();
+        const username = this.username.current.value;
+    }
+    render() {
+        return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={this,this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label><input autoFocus ref={this.username} id="username" type="text" className="from-consol"/>
+                    </div>
+            
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label><input id="password" type="text" className="from-consol"/>
+                    </div>
+                    <button className="btn btn-primary">Login</button>
+            </form>
+        </div>
     );
   }
 }
